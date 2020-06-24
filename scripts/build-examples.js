a79c75b1bd00ca9dacce2a8c0a9d3807cc39d52e
Jake Richards <jake.richards@genesys.com>
2020-06-24 08:12:59 -0400

    Expose examples npm script for jenkins

--- scripts/build-examples.js
+++ scripts/build-examples.js
@@ -1,27 +1,32 @@
 /* eslint-env node */
-import pkg from '../package.json';
-import fs from 'fs-extra';
-import path from 'path';
-import glob from 'glob';
+const pkg = require('../package.json');
+const fs = require('fs-extra');
+const path = require('path');
+const glob = require('glob');
 
 const { PC_OAUTH_CLIENT_IDS } = process.env;
 const BROWSER_FILENAME = `${pkg.name}.js`;
 
-const transformExampleSdkUrl = (buffer) => {
+// Called from command line
+if (!module.parent) {
+    buildExamples('dist-examples');
+}
+
+function transformExampleSdkUrl(buffer) {
     return buffer.replace(
         /(\s*<script.*src=")([^"]+client-app-sdk[^"]+)(".*<\/script>\s*)/i,
         '$1' + (BROWSER_FILENAME || '$2') + '$3\n'
     );
-};
+}
 
-const transformSdkOAuthClientIds = (buffer) => {
+function transformSdkOAuthClientIds(buffer) {
     return buffer.replace(
         /(pcOAuthClientIds =)[^;]+;/,
         `$1${PC_OAUTH_CLIENT_IDS}`
     );
-};
+}
 
-export const buildExample = (outDir, relativeFilePath) => {
+function buildExample(outDir, relativeFilePath) {
     let buffer = fs.readFileSync(relativeFilePath, 'utf8');
     buffer = transformExampleSdkUrl(buffer);
     if (PC_OAUTH_CLIENT_IDS) {
@@ -31,8 +36,10 @@ export const buildExample = (outDir, relativeFilePath) => {
         path.join(outDir, relativeFilePath.replace('examples/', '')),
         buffer
     );
-};
+}
 
-export const buildExamples = (outDir) => {
+function buildExamples(outDir) {
     glob.sync('examples/**/*', { nodir: true }).forEach(example => buildExample(outDir, example));
-};
+}
+
+module.exports = { buildExample, buildExamples };
