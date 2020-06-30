9d3833c3a0b003f69d38f784730f624d3d48fc24
Jake Richards <jake.richards@genesys.com>
2020-06-30 15:21:03 -0400

    Add umd bundle to index files and all example files

--- scripts/create-manifest.js
+++ scripts/create-manifest.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const glob = require('glob');
 
-const { WEB_APP_NAME, WEB_APP_VERSION, BUILD_NUMBER } = process.env;
+const { WEB_APP_NAME, WEB_APP_VERSION, BUILD_NUMBER, USE_BRANCH_SDK_IN_EXAMPLES } = process.env;
 if(!WEB_APP_NAME) throw new Error("WEB_APP_NAME environment variable not set");
 if(!WEB_APP_VERSION) throw new Error("WEB_APP_VERSION environment variable not set");
 if(!BUILD_NUMBER) throw new Error("BUILD_NUMBER environment variable not set");
@@ -13,6 +13,8 @@ fs.outputJSONSync(path.join(__dirname, "../dist-examples/manifest.json"), {
     version: WEB_APP_VERSION,
     build: BUILD_NUMBER,
     buildDate: new Date().toISOString(),
-    indexFiles: glob.sync('examples/**/*.html')
-        .map(file => ({ file: file.replace('examples/', '') }))
+    indexFiles: glob.sync('examples/**/*', { nodir: true })
+        .map(file => file.replace('examples/', ''))
+        .concat(USE_BRANCH_SDK_IN_EXAMPLES === "true" ? ['purecloud-client-app-sdk.js'] : [])
+        .map(file => ({ file }))
 }, { spaces: 2 });
