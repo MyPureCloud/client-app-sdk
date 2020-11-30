545357fcf7329ba488e205022fd91b8d002c65de
Jake Richards <jake.richards@genesys.com>
2020-11-30 16:06:18 -0500

    Add a generic ToastOptions type that enforces timeout / show close button requirements
    Fix toast docs.  Automatic dismissal is timeout > 0
    
    Make toast options easier to work with by allowing all properties to be optional (except special cases)
    
    Fix PR nits and small tweaks

--- src/modules/alerting.ts
+++ src/modules/alerting.ts
@@ -9,12 +9,6 @@ import BaseApi from './base';
 const VALID_MESSAGE_TYPES = ['error', 'info', 'success'] as const;
 const VALID_SUPPLEMENTAL_OPTIONS = ['id', 'markdownMessage', 'timeout', 'showCloseButton'] as const;
 
-interface BaseToastMessageProps {
-    title: string;
-    message: string;
-    type: ValidMessageType;
-}
-
 type ValidMessageType = typeof VALID_MESSAGE_TYPES[number];
 
 const isValidMessageType = (type: string): type is ValidMessageType => {
@@ -32,6 +26,42 @@ const pick = <
     return newObj;
 };
 
+type ToastOptions<Type extends ValidMessageType, Timeout extends number> = {
+    /**
+     * The id of the message.  Duplicate IDs will replace each other in the toast display.  All IDs will be
+     * namespaced with your app ID to avoid collisions.  (Default will just be your app's namespace and will not support multiple messages)
+     * */
+    id?: string;
+    /** Toast type, valid options are 'error', 'info', 'success'.  (Default is 'info') */
+    type?: Type;
+    /** Boolean indicating if the message is in MD.  (Default is true) */
+    markdownMessage?: boolean;
+} & ToastTimeoutConfig<Type, Timeout> & ToastCloseButtonConfig<Timeout>;
+
+type ToastTimeoutConfig<Type extends ValidMessageType, Timeout extends number> = Type extends 'error'
+    ? {
+        /**
+         * Time in seconds to show the toast.  Set to 0 to disable automatic dismissal. (Default is 5)
+         * Timeout must be 0 for toasts with type 'error'.
+         */
+        timeout: 0;
+    } : {
+        /** Time in seconds to show the toast.  Set to 0 to disable automatic dismissal. (Default is 5) */
+        timeout?: Timeout;
+    }
+
+type ToastCloseButtonConfig<Timeout extends number> = Timeout extends 0
+    ? {
+        /**
+         * Boolean indicating if the close button should be shown. (Default is false)
+         * Must show close button when `timeout` is 0.
+         */
+        showCloseButton: true 
+    } : { 
+        /** Boolean indicating if the close button should be shown. (Default is false) */
+        showCloseButton?: boolean
+    }
+
 /**
  * Handles aspects of alerting and attention of this app with Genesys Cloud
  *
@@ -43,7 +73,7 @@ class AlertingApi extends BaseApi {
      * Displays a toast popup.
      *
      * Permanent/Sticky toasts are not allowed.  Therefore, toasts must specify either a manual
-     * dismissal (`showCloseButton: true`) or an automatic dismissal (`timeout >= 0`). Both
+     * dismissal (`showCloseButton: true`) or an automatic dismissal (`timeout > 0`). Both
      * `showCloseButton` and `timeout` can be specified to provide both dismissal options.
      *
      * Error toasts (`type: 'error'`) require manual dismissal and must be specified with `showCloseButton: true`.
@@ -86,22 +116,15 @@ class AlertingApi extends BaseApi {
      *
      * @since 1.0.0
      */
-    showToastPopup(title: string, message: string, options: {
-        /**
-         * The id of the message.  Duplicate IDs will replace each other in the toast display.  All IDs will be
-         * namespaced with your app ID to avoid collisions.  (Default will just be your app's namespace and will not support multiple messages)
-         * */
-        id?: string;
-        /** Toast type, valid options are 'error', 'info', 'success'.  (Default is 'info') */
-        type?: 'error' | 'info' | 'success';
-        /** Boolean indicating if the message is in MD.  (Default is true) */
-        markdownMessage?: boolean;
-        /** Time in seconds to show the toast.  Set to 0 to disable automatic dismissal. (Default is 5) */
-        timeout?: number;
-        /** Boolean indicating if the close button should be shown. (Default is false) */
-        showCloseButton?: boolean;
-    } = {}) {
-        const messageParams: BaseToastMessageProps & typeof options = {
+    showToastPopup<
+        MessageType extends ValidMessageType,
+        Timeout extends number
+    >(
+        title: string,
+        message: string,
+        options: ToastOptions<MessageType, Timeout> = {} as ToastOptions<MessageType, Timeout>
+    ) {
+        const messageParams = {
             title,
             message,
             type: 'info'
