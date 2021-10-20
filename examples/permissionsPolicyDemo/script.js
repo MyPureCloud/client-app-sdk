a0bba57986ff2b09a663ba3d8a199a6eb62d7ae8
cparslow <caroline.parslow@genesys.com>
2021-10-20 09:15:25 -0400

    Add permissionsPolicy example app

---
+++ examples/permissionsPolicyDemo/script.js
@@ -0,0 +1,79 @@
+  const enterFullscreen = () => {
+    document.getElementById('fullscreenImage').requestFullscreen();
+  }
+  const clipboardWrite = () => {
+    const textInput = document.getElementById('clipboardTextInput').value;
+    const type = "text/plain";
+    const blob = new Blob([textInput], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    navigator.clipboard.write(data)
+  }
+  const clipboardWriteText = () => {
+    const textInput = document.getElementById('clipboardTextInput').value;
+    const type = "text/plain";
+    const blob = new Blob([textInput], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    navigator.clipboard.write(data)
+  }
+  const clipboardReadText = (trigger) => {
+    navigator.clipboard.readText().then((data) => {
+      const clipboardContents = document.getElementById('clipboard');
+      if (clipboardContents) {
+        clipboardContents.innerHTML = data;
+      }
+      const clipboardHistory = document.getElementById('clipboardHistory');
+      const text = document.createElement('p');
+      text.innerHTML = (trigger ? trigger : 'button trigger: ') + data;
+      clipboardHistory.prepend(text);
+    })
+  }
+  const clipboardRead = async (trigger) => {
+    const clipboardHistory = document.getElementById('clipboardHistory');
+    const clipboardImg = document.getElementById('clipboardImage');
+    navigator.clipboard.read().then(async (data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].types.includes('image/png')) {
+          data[i].getType('image/png').then((blob) => {
+            clipboardImg.src = URL.createObjectURL(blob);
+          });
+        } else if(data[i].types.includes('image/jpeg')) {
+          data[i].getType('image/jpeg').then((blob) => {
+            clipboardImg.src = URL.createObjectURL(blob);
+          });
+        } else if(data[i].types.includes('text/plain')){
+          data[i].getType('text/plain').then(async (blob) => {
+            const clipboardText = await blob.text();
+            const text = document.createElement('p');
+            text.innerHTML = (trigger ? trigger : 'button trigger: ') + clipboardText;
+            clipboardHistory.prepend(text);
+          });
+        }
+      }
+    })
+  }
+  document.addEventListener('copy', function() {
+    clipboardReadText('copy event: ')
+  }); 
+  document.addEventListener('paste', function() {
+    clipboardReadText('paste event: ')
+  }); 
+  const displayCapture = async () => {
+    let captureStream = null;
+
+    try {
+      captureStream = await navigator.mediaDevices.getDisplayMedia();
+    } catch(err) {
+      console.error("Error: " + err);
+    }
+    return captureStream;
+  }
+ 
+  // To test if permission requires focus: Call the following function
+  // with the function you want to test, refresh the page,
+  // and click anywhere in the top level page.
+  const testUnfocusedPermission = (functionToTest) => {
+    setTimeout(() => {
+      console.log('Testing without iframe focus');
+      functionToTest();
+    }, 5000)
+  };
