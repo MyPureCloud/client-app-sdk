224a88a2234ffc53ae171cbeb35d1f7546ddcad6
justinaray <justin.a.ray@gmail.com>
2021-12-14 23:39:29 -0500

    ATEAM-603: Improvements to groups example
    Move clickable nodes from lis to anchors for a11y
    Set group names as text nodes to avoid XSS attacks
    Limit fetch and display of groups to a max of 10
    Use a template to simplify rendering of group lis
    
    Fix linting errors in directory module

--- src/modules/directory.ts
+++ src/modules/directory.ts
@@ -13,35 +13,35 @@ import BaseApi from './base';
  * @since 2.3.0
  */
 class DirectoryApi extends BaseApi {
-  /**
-   * Shows the profile of a specified user
-   *
-   * ```ts
-   * myClientApp.directory.showUser("targetUserId");
-   * ```
-   *
-   * @param userId - The id of the user to show
-   *
-   * @since 2.3.0
-   */
-  showUser(userId: string) {
-    super.sendMsgToPc('showProfile', { profileId: userId });
-  }
+    /**
+     * Shows the profile of a specified user
+     *
+     * ```ts
+     * myClientApp.directory.showUser("targetUserId");
+     * ```
+     *
+     * @param userId - The id of the user to show
+     *
+     * @since 2.3.0
+     */
+    showUser(userId: string) {
+        super.sendMsgToPc('showProfile', { profileId: userId });
+    }
 
-  /**
-   * Shows the specified group
-   *
-   * ```ts
-   * myClientApp.directory.showGroup("targetGroupId");
-   * ```
-   *
-   * @param groupId - The id of the group to show
-   *
-   * @since 2.3.0
-   */
-  showGroup(groupId: string) {
-    super.sendMsgToPc('showGroup', { groupId: groupId });
-  }
+    /**
+     * Shows the specified group
+     *
+     * ```ts
+     * myClientApp.directory.showGroup("targetGroupId");
+     * ```
+     *
+     * @param groupId - The id of the group to show
+     *
+     * @since 2.3.0
+     */
+    showGroup(groupId: string) {
+        super.sendMsgToPc('showGroup', { groupId: groupId });
+    }
 }
 
 export default DirectoryApi;
